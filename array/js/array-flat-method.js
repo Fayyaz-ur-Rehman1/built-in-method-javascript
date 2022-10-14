a4cf49b157efd1bde731b89a166b024082732857
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-10-14 22:25:35 +0530

    inbuilt-method

--- array/js/array-flat-method.js
+++ array/js/array-flat-method.js
@@ -1,7 +1,7 @@
-var  a = [1,2,3,[[[8,[4,5],7]]]]
-var b = a.flat(Infinity)
+var  d = [1,2,3,[[[8,[4,5],7]]]]
+var b = d.flat(Infinity)
 console.log(b)
 
-var  a = [10,26,34,[[[84,[43,56],75]]]]
-var b = a.flat()
+var  d = [10,26,34,[[[84,[43,56],75]]]]
+var b = d.flat()
 console.log(b)
