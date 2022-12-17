69a56ad494ea4ca18f5892c1ddbac873662ba677
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-12-17 17:26:17 +0530

    Built-In-method

--- string/js/string-slice-method.js
+++ string/js/string-slice-method.js
@@ -1,3 +1,7 @@
 var a = "hlw i m Fayyaz Ur Rehman"
-var b = a.slice(2,5)
+var b = a.slice(1,9)
 document.write(b)
+document.write("<br>")
+var c = "hello my all freind"
+var d = c.slice(4,11)
+document.write(d)
