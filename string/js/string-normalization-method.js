dae4b82effd24e599c436dc92564734907ccefd9
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-12-19 17:09:39 +0530

    Built-In-method

--- string/js/string-normalization-method.js
+++ string/js/string-normalization-method.js
@@ -1,7 +1,11 @@
 var a = 'fayyaz'
-var b = 'fayyaz'
+var b = 'fayaz'
 document.write(a.length === b.length)
-
+document.write("<br>")
 var aa = a.normalize('NFD')
 var bb = a.normalize('NFD')
 document.write(aa.length === bb.length)
+document.write("<br>")
+var c = a.normalize('NFKC')
+var d = a.normalize('NFKC')
+document.write(c.length=== d.length)
