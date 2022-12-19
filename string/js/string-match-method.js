5526f2e79d6479117d42c89640a592a801d87d87
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-12-19 17:05:44 +0530

    Built-In-method

--- string/js/string-match-method.js
+++ string/js/string-match-method.js
@@ -1,3 +1,7 @@
 var a = "hlw i m Fayyaz Ur Rehman"
 var b = /[A-Z]/g
 document.write(a.match(b))
+document.write("<br>")
+var c = "My Name Is Fayyaz Ur Rehmen"
+var d = /[A-Z]/g
+document.write(c.match(d))
