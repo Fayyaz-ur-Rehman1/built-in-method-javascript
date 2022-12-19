2bf23a202bb7c7af04dcf9c49948393259fb9d6f
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-12-19 17:14:19 +0530

    Built-In-method

--- string/js/string-padStart-method.js
+++ string/js/string-padStart-method.js
@@ -1,2 +1,8 @@
 var a = "hlw"
 document.write(a.padStart(10 ,"+"))
+document.write("<br>")
+var a = "hlw"
+document.write(a.padStart(30 ,"/"))
+document.write("<br>")
+var a = "hlw"
+document.write(a.padStart(15 ,"-"))
