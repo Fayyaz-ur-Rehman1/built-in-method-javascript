5e6d4832396c2ab795cf627d37159afd7c190799
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-12-19 17:21:30 +0530

    Built-In-method

--- string/js/string-replaceAll-method.js
+++ string/js/string-replaceAll-method.js
@@ -1,2 +1,5 @@
 var a = "i m fayyaz and i m fayyaz going to fayyaz"
 document.write(a.replaceAll("fayyaz" , "Fayyaz-ur-rehman"))
+document.write("<br>")
+var a = "hello guys hello people hello freind"
+document.write(a.replaceAll("hello" , "Hii"))
