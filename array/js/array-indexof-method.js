a94e4d1af557f26eebb0cbfaa4664a8d64fccb26
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-12-21 05:00:38 +0530

    Built-In-method

---
+++ array/js/array-indexof-method.js
@@ -0,0 +1,2 @@
+var a = [1,2,3,4,5,67,8,9,10]
+document.write(a.indexOf())
---
+++ array/js/array-isarray-method.js
@@ -0,0 +1,7 @@
+var a = ['fayyaz']
+var b = Array.isArray(a)
+document.write(b + "<br>")
+
+var c = "45"
+var d = Array.isArray(c)
+document.write(d)
