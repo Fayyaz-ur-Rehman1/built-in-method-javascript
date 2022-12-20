08a23a06a0175b0559148c0045853b02fb2153c1
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-12-21 04:50:40 +0530

    Built-In-method

--- array/js/array-form-method.js
+++ array/js/array-form-method.js
@@ -1,3 +1,2 @@
 document.write(Array.from('fayyaz'));
 
-document.write(Array.from[1,2,3] , x => x + x );
---
+++ array/js/array-include-method.js
@@ -0,0 +1,5 @@
+var a = ['fayyaz','khilji','uzair','ayyaz']
+document.write(a.includes("fayyaz") + "<br>")
+
+var b = ['fayyaz','khilji','uzair','ayyaz']
+document.write(a.includes("fayya"))
