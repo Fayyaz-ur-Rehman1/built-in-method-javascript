83d41d68c1664e426cca1a57e137421085639384
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-12-21 04:04:02 +0530

    Built-In-method

--- array/js/array-findlastindex-method.js
+++ array/js/array-findlastindex-method.js
@@ -1,7 +1,15 @@
+// var a = [10,20,30,40,50,60,70,80,90,100]
+// document.write(a+ "<br>")
+// var b = a.findLastIndex(fayyaz)
+// function fayyaz(a){
+//     return  a <= 50
+// }
+// document.write(b)
+
 var a = [10,20,30,40,50,60,70,80,90,100]
 document.write(a+ "<br>")
-var b = a.findLastIndex(fayyaz)
+var b = a.findLast(fayyaz)
 function fayyaz(a){
-    return  a <= 50
+    return  a < 50
 }
 document.write(b)
