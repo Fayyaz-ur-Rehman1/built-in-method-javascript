857ebd2328549fb2820002e01ec2181d4df24be1
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-12-21 16:36:18 +0530

    Built-In-method

--- string/js/string-charCodeAt-method.js
+++ string/js/string-charCodeAt-method.js
@@ -1,2 +1,6 @@
 var a = "hlw i m fayyaz"
-document.write(a.charCodeAt(6));
+document.write(a.charCodeAt(7));
+// show sky table
+document.write("<br>")
+var a = "hlw i m fayyaz"
+document.write(a.charCodeAt(9));
