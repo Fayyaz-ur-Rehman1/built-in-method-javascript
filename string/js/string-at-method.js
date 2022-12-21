c2e7be3e41fc8de02c6cef10a683a88a3c40fbbc
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-12-21 16:30:55 +0530

    Built-In-method

--- string/js/string-at-method.js
+++ string/js/string-at-method.js
@@ -1,3 +1,6 @@
 var a = "hlw i m fayyaz ur rehman";
-document.write(a.at(6));
+document.write(a.at(8));
 // what is the value of (a) at index 6
+document.write("<br>")
+var a = "hlwguys";
+document.write(a.at(6));
