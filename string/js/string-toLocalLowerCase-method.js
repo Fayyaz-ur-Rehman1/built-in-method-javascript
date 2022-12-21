6474675e3c49b35c498e5770d872b2af6ac6e1d6
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-12-21 19:16:17 +0530

    Built-In-method

--- string/js/string-toLocalLowerCase-method.js
+++ string/js/string-toLocalLowerCase-method.js
@@ -1,3 +1,7 @@
 var a = "hlw i m Fayyaz Ur Rehman"
 var b = a.toLocaleLowerCase()
 document.write(b)
+document.write("<br>")
+var c = "HLW I AM FAYYAZURREHMAN"
+var d = a.toLocaleLowerCase()
+document.write(d)
