975f36e436ecabc556a53ebb785c297a2994c04c
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-12-21 19:21:12 +0530

    Built-In-method

--- string/js/string-toLowerCase-method.js
+++ string/js/string-toLowerCase-method.js
@@ -1,2 +1,5 @@
 var a = "hlw i m Fayyaz Ur Rehman"
 document.write(a.toLowerCase())
+document.write("<br>")
+var b = "hlw i m Fayyaz Ur Rehman"
+document.write(b.toLowerCase())
--- string/js/string-toString-method.js
+++ string/js/string-toString-method.js
@@ -2,3 +2,8 @@ var a = 17
 var b = a.toString()
 document.write(b)
 document.write(typeof b)
+document.write("<br>")
+var c = ["fayyaz"]
+var d = c.toString()
+document.write(d)
+document.write(typeof d)
