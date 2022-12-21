0b9cb736994db4615d93900ead08c39c8a808416
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-12-21 16:50:30 +0530

    Built-In-method

--- string/js/string-lastIndexOf-method.js
+++ string/js/string-lastIndexOf-method.js
@@ -1,2 +1,5 @@
-var a = "hlw i m fayyaz ur rehman fayyaz"
+var a = "hlw i m fayyaz ur rehman fayyaz "
 document.write(a.lastIndexOf("fayyaz"))
+document.write("<br>")
+var a = "hlw i m fayyaz ur rehman fayyaz rehman "
+document.write(a.lastIndexOf("rehman"))
