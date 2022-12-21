ef9fa8038cd63eb09f147be67b9d0c29a18cebdb
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-12-21 19:12:51 +0530

    Built-In-method

--- string/js/string-substring-method.js
+++ string/js/string-substring-method.js
@@ -1,2 +1,5 @@
 var a = "hlw i m Fayyaz Ur Rehman"
-document.write(a.substring(1,3))
+document.write(a.substring(2,7))
+document.write("<br>")
+var b = "hlw i m Fayyaz Ur Rehman"
+document.write(b.substring(6,10))
