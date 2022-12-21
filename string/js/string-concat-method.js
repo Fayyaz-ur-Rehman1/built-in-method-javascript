96562f811a6eae0f724e6de16fb8affd2774d182
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-12-21 16:40:27 +0530

    Built-In-method

--- string/js/string-concat-method.js
+++ string/js/string-concat-method.js
@@ -1,3 +1,9 @@
 var a = "hlw i m fayyaz"
 var b = "-ur-rehman"
 document.write(a.concat(b))
+
+document.write("<br>")
+
+var c = "hlw i m fayyaz khilji"
+var d = "-ur-rehman khan"
+document.write(c.concat(d))
