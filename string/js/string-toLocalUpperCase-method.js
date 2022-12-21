b480a710d2a21a2247f27215bc897f369aea8bbe
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-12-21 19:17:21 +0530

    Built-In-method

--- string/js/string-toLocalUpperCase-method.js
+++ string/js/string-toLocalUpperCase-method.js
@@ -1,3 +1,7 @@
 var a = "hlw i m Fayyaz Ur Rehman"
 var b = a.toLocaleUpperCase()
 document.write(b)
+document.write("<br>")
+var a = "hlw i m fayyaz ur rehman"
+var b = a.toLocaleUpperCase()
+document.write(b)
