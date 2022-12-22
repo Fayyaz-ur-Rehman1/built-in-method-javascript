f95a3d67878d8cec5b6fdd153d9880d77c3259ab
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-12-22 14:15:12 +0530

    Built-In-method

--- array/js/array-reduceright-method.js
+++ array/js/array-reduceright-method.js
@@ -1,4 +1,4 @@
-var a = [[1,2],[3,4],[5,8]]
+var a = [[1,2],[3,4],[5,6]]
 var b = a.reverse()
 var fay = a.reduceRight((b,c) => b.concat(c));
 document.write(fay)
--- array/js/array-value-method.js
+++
@@ -1,5 +0,0 @@
-var a = ['a','b','c'];
-var b = a.values()
-for(var c of b){
-    console.log(c)
-}
