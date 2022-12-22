dbfd020ef86a56c8ffed4e5a02dacbec7d3ea540
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-12-22 13:09:41 +0530

    Built-In-method

--- string/js/string-toUpperCase-method.js
+++ string/js/string-toUpperCase-method.js
@@ -1,2 +1,5 @@
 var a = "hlw i m Fayyaz Ur Rehman"
 document.write(a.toUpperCase())
+document.write("<br>")
+var a = "hlw i m Fayyaz Ur Rehman"
+document.write(a.toUpperCase())
--- string/js/string-trimEnd-method.js
+++ string/js/string-trimEnd-method.js
@@ -1,2 +1,5 @@
 var a = "hlw i m Fayyaz Ur Rehman       "
 alert(a.trimEnd())
+
+var b = "fayyaz                  "
+alert(b.trimEnd())
