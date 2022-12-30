50a61413f433c14d6d3015f69a744907270285ac
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-12-30 22:21:29 +0530

    inbuilt-method

--- array/js/array-push-method.js
+++ array/js/array-push-method.js
@@ -1,3 +1,3 @@
-var a = ['fayyaz','khilji','uzair','ayyaz']
-a.push('my-name-is-fayyaz');
-document.write(a)
+var b = ['fayyaz','khilji','uzair','ayyaz']
+b.push('my-name-is-fayyaz');
+document.write(b)
