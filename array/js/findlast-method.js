7af9a5b47861a29d6e825604324595af09d9d6c8
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2022-12-30 22:23:54 +0530

    inbuilt-method

--- array/js/findlast-method.js
+++ array/js/findlast-method.js
@@ -1,12 +1,12 @@
-var a = [5,10,20,30,40,50,60,70,80,90];
+var c = [5,10,20,30,40,50,60,70,80,90];
 
-var b = a.findlast(fayyaz)
+var b = c.findlast(fayyaz)
 function fayyaz(urrehman){
   return urrehman < 75
 }
-var a = [1,2,3,4,5,6,7,8,9,10];
-var b = a.findLast(fay);
-function fay(a){
-  return a < 9
+var c = [1,2,3,4,5,6,7,8,9,10];
+var b = c.findLast(fay);
+function fay(c){
+  return c < 9
 }
 document.write(b)
