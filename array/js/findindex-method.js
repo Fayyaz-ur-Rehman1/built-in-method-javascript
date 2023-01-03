c45721caa30e2f7686b0c1f71dbd12f12bc10ab0
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-01-03 22:12:32 +0530

    inbuilt-method

--- array/js/findindex-method.js
+++ array/js/findindex-method.js
@@ -1,9 +1,9 @@
 
 var a = [5,10,20,30,40,50,60,70,80,90];
 
-var b = a.findIndex(ayyaz)
-function ayyaz(urrehman){
-  return urrehman > 22
+var b = a.findIndex(fayyaz)
+function fayyaz(rehman){
+  return rehman > 22
 }
 
 document.write(b)
