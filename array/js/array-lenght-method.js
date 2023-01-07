b3d366456e750146264d1cb182230b6a2bdbefb5
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-01-07 14:03:50 +0530

    built-method

--- array/js/array-lenght-method.js
+++ array/js/array-lenght-method.js
@@ -6,3 +6,7 @@ document.write("<br>")
 
 const s = ['saif','Arbaz','mikail','hamza']
 document.write(s.length);
+document.write("<br>")
+
+const w = ['fayyaz','danish','uzair']
+document.write(w.length);
