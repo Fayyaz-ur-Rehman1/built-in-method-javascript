171d37f98d4702887a816819b3307c23eb283403
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-01-09 02:35:17 +0530

    inbuilt-method

--- array/js/array-find-method.js
+++ array/js/array-find-method.js
@@ -5,3 +5,11 @@ function fayyaz(urrehman){
   return urrehman > 45
 }
 document.write(b)
+
+document.write("<br>")
+
+ var c = [5,10,20,30,40,50,60,70,80,90]
+ var d = c.find(function (a){
+  return a > 34
+ })
+ document.write(d)
