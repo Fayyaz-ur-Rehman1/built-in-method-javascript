64dd2c5febc4e1b21340fbd9bdccbb02fdf8e3f9
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-01-09 04:05:02 +0530

    inbuilt-method

--- array/js/array-flatmap-method.js
+++
@@ -1,7 +0,0 @@
-var a = [3,2,[3,4],5,[6,7],8,[9,10]]
-var b =  a.flatMap(fayyaz)
-
-function fayyaz(a){
-    return a > 2
-}
-console.log(b)
--- array/js/array-reduse-method.js
+++ array/js/array-reduse-method.js
@@ -12,3 +12,19 @@
  function khilji(c,d){
     return c + d
  }
+
+ document.write("<br>")
+
+ var a = [1,2,3,43,5,46]
+ var b = a.reduce(function(a,b){
+   return a * b
+ })
+ document.write(b)
+
+document.write("<br>")
+
+ var a = [10.5,3.4,1.1,6.7];
+ var b = a.reduce(function(a,b){
+   return a + b
+ })
+ document.write(b)
