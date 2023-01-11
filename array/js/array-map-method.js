ce5b156b1842b169de4661edb3ee388a792e8e49
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-01-12 04:14:45 +0530

    inbuilt-method

--- array/js/array-map-method.js
+++ array/js/array-map-method.js
@@ -1,34 +1,40 @@
-var a = [1,2,3,4,5,6,7,8,9,10]
-var b = a.map( fayyaz);
-var c = a.map(function (eachVal ){
-    return eachVal + 1
-});
-var d = a.map((eachVal) => {
-    return eachVal +3
-})
-document.write(b)
-document.write("<br>")
+// var a = [1,2,3,4,5,6,7,8,9,10]
+// var b = a.map( fayyaz);
+// var c = a.map(function (eachVal ){
+//     return eachVal + 1
+// });
+// var d = a.map((eachVal) => {
+//     return eachVal +3
+// })
+// document.write(b)
+// document.write("<br>")
 
-function fayyaz(a){
-    return a * 3
-}
-// function khan(a){
-
+// function fayyaz(a){
+//     return a * 3
 // }
-// khan(1)
+// // function khan(a){
 
-var a = {
-    b : "khilji",
-    c: () => {
-        console.log('hello');
-    } 
-}
+// // }
+// // khan(1)
 
-a.c();
+// var a = {
+//     b : "khilji",
+//     c: () => {
+//         console.log('hello');
+//     } 
+// }
+
+// a.c();
 
 
-var a = [1,2,3,4,5,6,7,8,9,10]
-var c = a.map(function (eachVal ){
-    return eachVal + 1
-});
-document.write(c)
+// var a = [1,2,3,4,5,6,7,8,9,10]
+// var c = a.map(function (eachVal ){
+//     return eachVal + 1
+// });
+// document.write(c)
+
+var k =  [23,4,5,6,7,67,67,8,8,7,87]
+var j = a.map(function(a){
+    return a * 3
+})
+document.write(j)
