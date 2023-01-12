e0ebc2acb4b6cb97ad6d2d3bf9c62382e87a600a
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-01-13 01:43:42 +0530

    inbuilt-method

--- array/js/array-every-method.js
+++ array/js/array-every-method.js
@@ -1,15 +1,21 @@
-const a = [2,3,4,5,6,7,8,9,10];
-document.write(a+"<br>")
-document.write(a.every(b))
+// const a = [2,3,4,5,6,7,8,9,10];
+// document.write(a+"<br>")
+// document.write(a.every(b))
 
-function b(a){
-    return a >= 1
-}
-document.write("<hr>")
-const c = [10,20,30,40,50,60,70,80,90]
-document.write(c +"<br>")
-document.write(c.every(d))
+// function b(a){
+//     return a >= 1
+// }
+// document.write("<hr>")
+// const c = [10,20,30,40,50,60,70,80,90]
+// document.write(c +"<br>")
+// document.write(c.every(d))
 
-function d(c){
-    return c <= 100
-}
+// function d(c){
+//     return c <= 100
+// }
+
+var a = [1,2,3,4,5,6,7]
+document.write(a.every(function(d){
+    return d < 8
+}))
+document.write(a)
