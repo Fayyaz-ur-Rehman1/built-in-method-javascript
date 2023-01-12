76b0fb554391152072175fdf6b06b356794753aa
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-01-13 02:32:06 +0530

    inbuilt-method

--- array/js/array-filter-method.js
+++ array/js/array-filter-method.js
@@ -4,3 +4,11 @@ function fay(a){
   return a > 7
 }
 document.write(b)
+
+document.write("<br>")
+
+var d = [10,20,30,40,50,60,70,80,90]
+var c = d.filter(function(a){
+  return a > 34
+}) 
+document.write(c)
