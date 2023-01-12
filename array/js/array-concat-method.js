80797ba6a1a93666776fb34f4fd7e02a1b0ccef5
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-01-13 01:39:51 +0530

    inbuilt-method

--- array/js/array-concat-method.js
+++ array/js/array-concat-method.js
@@ -7,3 +7,8 @@ const g = ['fayyaz','danish']
 const v = ['uzair','saif']
 const k = (g.concat(v))
 document.write(k)
+
+document.write("<br>")
+var f = ['my','name','is']
+var d = ['fayyaz','ur','rehman']
+document.write(f.concat(d))
--- array/js/array-entries-method.js
+++ array/js/array-entries-method.js
@@ -1,21 +1,30 @@
-const fayyaz = ['fayyaz','danish','adnan','uzair']
-document.write(fayyaz+"<br>")
-const danish = fayyaz.entries();
-document.write(danish.next().value + "<br>");
-document.write(danish.next().value + "<br>");
-document.write(danish.next().value + "<br>");
-document.write(danish.next().value + "<br>");
+// const fayyaz = ['fayyaz','danish','adnan','uzair']
+// document.write(fayyaz+"<br>")
+// const danish = fayyaz.entries();
+// document.write(danish.next().value + "<br>");
+// document.write(danish.next().value + "<br>");
+// document.write(danish.next().value + "<br>");
+// document.write(danish.next().value + "<br>");
 
 
-document.write("<hr>")
+// document.write("<hr>")
 
 
-var a = ['my','name','fayyaz','khilji'];
-var b = a.entries(a + "<br>");
+// var a = ['my','name','fayyaz','khilji'];
+// var b = a.entries(a + "<br>");
 
-document.write(b.next().value + "<br>");
-document.write(b.next().value + "<br>");
-document.write(b.next().value + "<br>");
-document.write(b.next().value + "<br>");
-document.write(b.next().value + "<br>");
-document.write(b.next().value + "<br>");
+// document.write(b.next().value + "<br>");
+// document.write(b.next().value + "<br>");
+// document.write(b.next().value + "<br>");
+// document.write(b.next().value + "<br>");
+// document.write(b.next().value + "<br>");
+// document.write(b.next().value + "<br>");
+
+document.write("<br>" + '<hr>')
+
+var c = ['fayyaz','khilji','urrehman','khan']
+var b = c.entries(c + "<br>")
+document.write(b.next().value + "<br>")
+document.write(b.next().value + "<br>")
+document.write(b.next().value + "<br>")
+document.write(b.next().value + "<br>")
